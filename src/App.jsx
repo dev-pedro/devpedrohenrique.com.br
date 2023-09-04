59e9588d7601fc52d4d8d4a015b121c0bdafce55
dev-pedro <dev.pedro.rjas@gmail.com>
2023-09-04 11:14:00 -0300

    Novo projeto de site para o portfólio

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold font-serif">Pedro Henrique</h1>
+      <p className="text-blue-500">Bem vindo ao seu site!</p>
+    </>
+  )
+}
+
+export default App
